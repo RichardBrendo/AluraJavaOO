33250db8e10ed8b69bdd5b57c8616ac91598b4ec
RichardBrendo <brndxv@gmail.com>
2022-04-06 15:23:44 -0300

    Sintaxe-basica

---
+++ sintaxe-basica/src/testaCaracteres.java
@@ -0,0 +1,20 @@
+
+public class testaCaracteres {
+
+	public static void main(String[] args) {
+		char letra = 'a';
+		System.out.println(letra);
+		
+		char valor = 66;
+		System.out.println(valor);
+		
+		valor =(char) (valor + 1);
+		System.out.println(valor);
+		
+		String plavra = "alura curso online";
+		System.out.println(plavra);
+		
+		
+		
+	}
+}
